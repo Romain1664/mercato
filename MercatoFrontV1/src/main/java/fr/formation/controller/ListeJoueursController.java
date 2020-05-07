96f41ade1f92e1881f3042dd73b02409d4631eb7
Xalbat_Itoiz <xalbat.itoiz@gmail.com>
2020-05-07 12:15:45 +0200

    Modif

--- MercatoFrontV1/src/main/java/fr/formation/controller/ListeJoueursController.java
+++ MercatoFrontV1/src/main/java/fr/formation/controller/ListeJoueursController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import fr.formation.daoSpring.IDAOJoueur;
@@ -16,9 +17,10 @@ public class ListeJoueursController {
 	private IDAOJoueur daoJoueur;
 	
 	@GetMapping("/Liste_Joueurs")
-	public String listeJoueurs() {
+	public String listeJoueurs(Model model) {
 		
 		List<Joueur> joueurs = daoJoueur.findAll();
+		model.addAttribute("joueurs",joueurs);
 		
 		return "joueurs";
 	}
