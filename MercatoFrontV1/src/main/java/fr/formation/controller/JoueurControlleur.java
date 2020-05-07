a62a3b73cd08b1c605c32fbc08d0ec854af35ef9
Xalbat_Itoiz <xalbat.itoiz@gmail.com>
2020-05-07 12:13:47 +0200

    Joueur et Liste Joueurs

--- MercatoFrontV1/src/main/java/fr/formation/controller/JoueurControlleur.java
+++ MercatoFrontV1/src/main/java/fr/formation/controller/JoueurControlleur.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.formation.daoSpring.IDAOJoueur;
 import fr.formation.model.Compte;
@@ -18,14 +20,17 @@ public class JoueurControlleur {
 	@Autowired
 	private IDAOJoueur daoJoueur;
 	
+	
 	@GetMapping("/Menu_Joueur")
-	public String accueilJoueur()
+	public String accueilJoueur(HttpSession session,Model model)
 	{
+		model.addAttribute("message",session.getAttribute("message"));
+		session.removeAttribute("message");
 		return "joueur";
 	}
 	
 	
-	@GetMapping("/joueur/afficherStats")
+	@GetMapping("/Menu_Joueur/afficherStats")
 	public String afficherStat(HttpSession session,Model model) 
 	{
 		Compte c = (Compte) session.getAttribute("compte");
@@ -33,10 +38,10 @@ public class JoueurControlleur {
 
 		model.addAttribute("joueur",j);
 		
-		return "statAfficher";
+		return "statsAfficher";
 	}
 	
-	@GetMapping("/joueur/modifierStats")
+	@GetMapping("/Menu_Joueur/modifierStats")
 	public String modifierStat(HttpSession session,Model model) 
 	{
 		Compte c = (Compte) session.getAttribute("compte");
@@ -44,7 +49,33 @@ public class JoueurControlleur {
 		
 		model.addAttribute("joueur",j);
 		
-		return "statModifier";
+		return "statsModifier";
+	}
+	
+	@PostMapping("/Menu_Joueur/ModificationStat")
+	public String modificationStat(
+			@RequestParam(value="tir") Integer tir,
+			@RequestParam(value="precision") Integer precision,
+			@RequestParam(value="acceleration") Integer acceleration,
+			@RequestParam(value="puissance") Integer puissance,
+			@RequestParam(value="tacle") Integer tacle,
+			@RequestParam(value="marquage") Integer marquage,
+			HttpSession session,Model model) 
+	{
+		Compte c = (Compte) session.getAttribute("compte");
+		Joueur j = daoJoueur.findById(c.getId()).get();
+		
+		j.setTir(tir);
+		j.setPrecision(precision);
+		j.setAcceleration(acceleration);
+		j.setPuissance(puissance);
+		j.setTacle(tacle);
+		j.setMarquage(marquage);
+		
+		daoJoueur.save(j);
+		session.setAttribute("message", "Vos stats ont été modifiées");
+		
+		return "redirect:/Menu_Joueur";
 	}
 	
 	@GetMapping("/joueur/retraite")
--- MercatoFrontV1/src/main/java/fr/formation/controller/ListeJoueursController.java
+++ MercatoFrontV1/src/main/java/fr/formation/controller/ListeJoueursController.java
@@ -1,9 +1,13 @@
 package fr.formation.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import fr.formation.daoSpring.IDAOJoueur;
+import fr.formation.model.Joueur;
 
 @Controller
 public class ListeJoueursController {
@@ -11,7 +15,12 @@ public class ListeJoueursController {
 	@Autowired
 	private IDAOJoueur daoJoueur;
 	
-	
-
+	@GetMapping("/Liste_Joueurs")
+	public String listeJoueurs() {
+		
+		List<Joueur> joueurs = daoJoueur.findAll();
+		
+		return "joueurs";
+	}
 	
 }
