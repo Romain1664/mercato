eff98a9afc39df8b2b610f52733625f58b990126
Romain1664 <roomain.s.31@gmail.com>
2020-05-14 14:44:22 +0200

    song

--- Mercato-Spring-Boot/src/main/java/fr/formation/exception/JoueurValidationException.java
+++ Mercato-Spring-Boot/src/main/java/fr/formation/exception/JoueurValidationException.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Le joueur n'a pu etre sélectionné")
 public class JoueurValidationException extends RuntimeException  {
 
+	
 	private static final long serialVersionUID = 1L;
 }
